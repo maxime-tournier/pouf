386c71eba3c76ea54fcfc985a175cac407c4ffa3
Maxime Tournier <maxime.tournier@brain.riken.jp>
2014-10-14 14:55:46 +0900

    fixed deadlock on worker start

--- thread/worker.cpp
+++ thread/worker.cpp
@@ -8,28 +8,39 @@ namespace thread {
   struct exit { };
   
 
-  worker::worker()
-	: thread( [&] { this->loop(); }) {
+  worker::worker() {
 	lock_type lock(mutex);
+	thread = std::thread( [&] { this->loop(); } );
+
+	// wait for thread to notify us
 	cv.wait(lock);
+
+	// lock is reacquired so we don't exit until thread is waiting on
+	// cv
   }
 
 
   void worker::loop() {
+	
+	// block until constructor is waiting on us
+	lock_type lock(mutex);
+
+	// notify constructor we started
+	cv.notify_one();
 
 	try {
-	  cv.notify_one();
+	  
+	  bool first = true;
 
 	  task_type task;
 	  while( true ) {
 		{
-		  lock_type lock(mutex);
-
-		  cv.wait(lock, [&] { return !queue.empty();});
+		  lock_type sub = first ? std::move(lock) : lock_type(mutex);
+		  cv.wait(sub, [&] { return !queue.empty();});
 		  task = queue.front();
 		  queue.pop();
+		  first = false;
 		}
-		// mutex.unlock();
 		
 		task();
 	  }
