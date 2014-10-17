7c9f28b947e70c8c46b408e37ffdee176588e4f6
Maxime Tournier <maxime.tournier@brain.riken.jp>
2014-10-17 11:37:26 +0900

    moved console upstream in SofaPython

--- scene/console.py
+++ scene/console.py
@@ -2,8 +2,16 @@ import Sofa
 from SofaPython import console
 
 def createScene(node):
-
     print "usage: as always in python, call dir() to see what's available."
     c = console.Console( locals() )
     
+    import atexit
+
+    def handler():
+        print 'handler'
+
+    atexit.register(handler)
+    
+
+
 
